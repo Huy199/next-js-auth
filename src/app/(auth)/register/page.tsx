d846fa344b68f70d4e5de4c78fe66655d5e4f464
Hoang Quang Huy <huy.hoangquang10@mobifone.vn>
2024-08-23 16:32:13 +0700

    first-commit

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+}
